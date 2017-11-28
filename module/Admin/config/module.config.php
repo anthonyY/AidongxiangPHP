cfbc97c1d328ac01d8865d2d63969baa1384f116
fishlink <992031717@qq.com>
2017-11-28 23:24:33 +0800

    新增代码

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -28,6 +28,128 @@
                         ],
                     ],
                 ],
+
+                //用户管理
+                'admin-user' => [
+                    'type'    => Segment::class,
+                    'options' => ['route' => MODULE_ADMIN . '/user[/a:action][/p:page][/i:id]',
+                        'constraints' => array(
+                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'page' => '[0-9]+'
+                        ),
+                        'defaults' => [
+                            'controller' => Controller\UserController::class,
+                            'action'     => 'index',
+                        ],
+                    ],
+                ],
+
+                'admin-business' => [
+                    'type'    => Segment::class,
+                    'options' => ['route' => MODULE_ADMIN . '/business[/a:action][/p:page][/i:id]',
+                        'constraints' => array(
+                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'page' => '[0-9]+'
+                        ),
+                        'defaults' => [
+                            'controller' => Controller\BusinessController::class,
+                            'action'     => 'adsList',
+                        ],
+                    ],
+                ],
+
+                'admin-video' => [
+                    'type'    => Segment::class,
+                    'options' => ['route' => MODULE_ADMIN . '/video[/a:action][/p:page][/i:id]',
+                        'constraints' => array(
+                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'page' => '[0-9]+'
+                        ),
+                        'defaults' => [
+                            'controller' => Controller\VideoController::class,
+                            'action'     => 'index',
+                        ],
+                    ],
+                ],
+
+                'admin-audio' => [
+                    'type'    => Segment::class,
+                    'options' => ['route' => MODULE_ADMIN . '/audio[/a:action][/p:page][/i:id]',
+                        'constraints' => array(
+                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'page' => '[0-9]+'
+                        ),
+                        'defaults' => [
+                            'controller' => Controller\AudioController::class,
+                            'action'     => 'index',
+                        ],
+                    ],
+                ],
+
+                'admin-microblog' => [
+                    'type'    => Segment::class,
+                    'options' => ['route' => MODULE_ADMIN . '/microblog[/a:action][/p:page][/i:id]',
+                        'constraints' => array(
+                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'page' => '[0-9]+'
+                        ),
+                        'defaults' => [
+                            'controller' => Controller\MicroblogController::class,
+                            'action'     => 'index',
+                        ],
+                    ],
+                ],
+
+                'admin-article' => [
+                    'type'    => Segment::class,
+                    'options' => ['route' => MODULE_ADMIN . '/article[/a:action][/p:page][/i:id]',
+                        'constraints' => array(
+                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'page' => '[0-9]+'
+                        ),
+                        'defaults' => [
+                            'controller' => Controller\ArticleController::class,
+                            'action'     => 'index',
+                        ],
+                    ],
+                ],
+
+                'admin-comment' => [
+                    'type'    => Segment::class,
+                    'options' => ['route' => MODULE_ADMIN . '/comment[/a:action][/p:page][/i:id]',
+                        'constraints' => array(
+                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'page' => '[0-9]+'
+                        ),
+                        'defaults' => [
+                            'controller' => Controller\CommentController::class,
+                            'action'     => 'index',
+                        ],
+                    ],
+                ],
+
+                'admin-finance' => [
+                    'type'    => Segment::class,
+                    'options' => ['route' => MODULE_ADMIN . '/finance[/a:action][/p:page][/i:id]',
+                        'constraints' => array(
+                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            'page' => '[0-9]+'
+                        ),
+                        'defaults' => [
+                            'controller' => Controller\FinanceController::class,
+                            'action'     => 'index',
+                        ],
+                    ],
+                ],
+
                 //管理模块
                 'admin-setting' => [
                     'type'    => Segment::class,
@@ -39,25 +161,11 @@
                         ),
                         'defaults' => [
                             'controller' => Controller\SettingController::class,
-                            'action'     => 'index',
+                            'action'     => 'adminList',
                         ],
                     ],
                 ],
-//                //用户管理
-//                'admin-user' => [
-//                    'type'    => Segment::class,
-//                    'options' => ['route' => MODULE_ADMIN . '/user[/a:action][/p:page][/i:id]',
-//                        'constraints' => array(
-//                            'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-//                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-//                            'page' => '[0-9]+'
-//                        ),
-//                        'defaults' => [
-//                            'controller' => Controller\UserController::class,
-//                            'action'     => 'index',
-//                        ],
-//                    ],
-//                ],
+
             ],
         ],
         'controller_plugins' => array(
@@ -69,6 +177,14 @@
             'factories' => [
                 Controller\IndexController::class => InvokableFactory::class,
                 Controller\SettingController::class => InvokableFactory::class,
+                Controller\ArticleController::class => InvokableFactory::class,
+                Controller\AudioController::class => InvokableFactory::class,
+                Controller\BusinessController::class => InvokableFactory::class,
+                Controller\CommentController::class => InvokableFactory::class,
+                Controller\FinanceController::class => InvokableFactory::class,
+                Controller\MicroblogController::class => InvokableFactory::class,
+                Controller\UserController::class => InvokableFactory::class,
+                Controller\VideoController::class => InvokableFactory::class,
             ],
         ],
         'view_manager' => [
---
+++ module/Admin/src/Controller/AudioController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Admin\Controller;
+
+use Admin\Controller\CommonController;
+use Zend\View\Model\ViewModel;
+
+class AudioController extends CommonController
+{
+
+}
