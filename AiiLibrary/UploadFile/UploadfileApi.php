a94fa6429b623f07303ac55c34722b1efb6bd978
MACHENIKE <992031717@qq.com>
2018-04-14 15:09:37 +0800

    更新代码

--- AiiLibrary/UploadFile/UploadfileApi.php
+++ AiiLibrary/UploadFile/UploadfileApi.php
@@ -290,7 +290,10 @@ class UploadfileApi
             $keyarr = array('name', 'type', 'tmp_name', 'error', 'size', 'data');
             foreach($keyarr as $r)
             {
-                eval("\$f['$r'] = \$this->_files['$key']['$r']$keystr;");
+                if(isset($this->_files[$key][$r]))
+                {
+                    eval("\$f['$r'] = \$this->_files['$key']['$r']$keystr;");
+                }
             }
             $newname = $this->_doUpload($f);
             eval("\$this->_files['$key']['new_name']$keystr = '$newname';");
